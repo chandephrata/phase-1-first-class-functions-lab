9f2da00ba7c30180958282c2901329104d2180cf
chandelierephrata <chandephrata@gmail.com>
2021-10-21 19:33:02 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return (drivers.slice(0,2))
+}
+const returnLastTwoDrivers= function(drivers){
+    return (drivers.slice(-2))
+}
+const createFareMultiplier= function(numberOfTickets){
+    return function(fare){
+    return numberOfTickets * fare
+}
+}
+const fareDoubler= createFareMultiplier(2)
+
+const fareTripler= createFareMultiplier(3)
+
+const selectDifferentDrivers= function(drivers){
+    return drivers.slice(-2)
+}
