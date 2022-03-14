35a3b3301b8f9c975329f8611aff250271387cb0
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-14 10:02:07 -0600

    commit tree fd957c980cfa64a635c877193d438fd77c9b90d9

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const returnFirstTwoDrivers = function(drivers){
 const returnLastTwoDrivers= function(drivers){
     return (drivers.slice(-2))
 }
+
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers]
+
 const createFareMultiplier= function(numberOfTickets){
     return function(fare){
     return numberOfTickets * fare
 }
 }
+
 const fareDoubler= createFareMultiplier(2)
 
 const fareTripler= createFareMultiplier(3)
 
-const selectDifferentDrivers= function(drivers){
-    return drivers.slice(-2)
+const selectDifferentDrivers= function (drivers,driversToReturn){
+    return driversToReturn(drivers);
 }
